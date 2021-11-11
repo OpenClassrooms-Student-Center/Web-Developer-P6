453d6817884da131e123066309f68eebaea3fe68
Jonathan Piboteau <jonathanpiboteau@gmail.com>
2021-11-11 13:19:48 +0100

    add images folders

--- backend/models/sauces.js
+++ backend/models/sauces.js
@@ -11,7 +11,7 @@ const saucesSchema = mongoose.Schema({
     likes: { type: Number, required: false, default: 0 },
     dislikes: { type: Number, required: false, default: 0 },
     usersLiked: { type: Array, required: false },
-    usersDisliked: { type: array, required: false }
+    usersDisliked: {type: Array, required: false },
 })
 
 module.exports = mongoose.model('sauces', saucesSchema);
