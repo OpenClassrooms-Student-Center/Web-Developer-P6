971dc4743d0c16f659f0d50b83a33e46331a5393
Jonathan Piboteau <jonathanpiboteau@gmail.com>
2021-11-11 17:11:55 +0100

    test2 dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,19 @@
 const express = require('express');
-require('dotenv').config;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+require('dotenv').config();
 
 const userRoutes = require('./routes/user'); // add user route
 const sauceRoutes = require('./routes/sauces'); //add sauce route
 
 // Connect to mongodb
-mongoose.connect(`${process.env.MONGO_DSN}`,{
-  useNewUrlParser: true,  //deprecation warnings = avertissement fonctionnalité,biblio existante va être modifiée,supprimée,remplacée//
-  useUnifiedTopology: true,})
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose.connect(process.env.DB_CODE, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+})
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 app.use(bodyParser.json());
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
+const dotenv = require('dotenv')
+dotenv.config();
 const http = require('http');
 const app = require('./app');
+
 
 const normalizePort = val => {
   const port = parseInt(val, 10);
