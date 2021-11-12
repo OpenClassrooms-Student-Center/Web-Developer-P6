446bdd3cd02f4b67227a37b6d04ec5c0f430106d
Jonathan Piboteau <jonathanpiboteau@gmail.com>
2021-11-12 11:36:19 +0100

    Manage dotenv, bodyparserdepreciation, plugin nocache & helmet, begin documentation

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,22 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken'); // Import JsonToken to protect route & verity authentification user to resquesting
 
+// This middleware will be applied to all routes to secure them
 module.exports = (req, res, next) => {
   try {
+    // Retrieve the token from the header of the authorization request, retrieving only the second element of the array (because split)
     const token = req.headers.authorization.split(' ')[1];
+    // We check the decoded token with the secret key initiated with the creation of the initially encoded token (Cf Controller user), the keys must match
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    // We verify that the userId sent with the request matches the userId encoded in the token
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
+      throw 'Identification utilisateur impossible'; // if the token does not match the userId: error
     } else {
-      next();
+      next(); // if everything is valid we go to the next middleware
     }
-  } catch {
+  } catch (error) { // authentication problem if error in registrations
     res.status(401).json({
-      error: new Error('Invalid request!')
-    });
+      error: error | 'Requête non authentifiée!'
+    })
   }
-};
+}
--- backend/models/user.js
+++ backend/models/user.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator') // package to control email unique validation
 
 const userSchema = mongoose.Schema({
-    email:{type: String, required: true, unique: true},
-    password:{type: String, required: true}
+    email:{type: String, required: true, unique: true, match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, "Veuillez entrer une adresse email correcte"]}, //use regular expression to manage email validation}
+    password:{type: String, required: true},
+    
 
 })
 
-userSchema.plugin(uniqueValidator);
-module.exports = mongoose.model('user', userSchema)
+userSchema.plugin(uniqueValidator); // using plugin unique validator
+module.exports = mongoose.model('user', userSchema) //export model to use app
