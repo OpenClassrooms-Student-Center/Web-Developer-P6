5bec55ab71c6952eb0d9cd6f2c44de08e28222cc
Tom Charmasson <tomcharmasson@gmail.com>
2021-11-25 18:57:21 +0100

    Etape 1 : Démarrer le serveur Backend
    - Installation Express
    - Installation Mongoose
    - Connexion MongoDB

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose
+  .connect("mongodb+srv://yinkyflash:uKRv6PJvX6SFfSs@cluster0.cfqd1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Successfully connected to MongoDB ! ✅ 🍀"))
+  .catch(() => console.log("MMMMM, Connection to MongoDB failed ! ❌ 🍀"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
+
+app.post("/api/stuff", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Objet créé !",
+  });
+});
+
+app.get("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl: "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 4900,
+      userId: "qsomihvqios",
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl: "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 2900,
+      userId: "qsomihvqios",
+    },
+  ];
+  res.status(200).json(stuff);
+});
+
+module.exports = app;
