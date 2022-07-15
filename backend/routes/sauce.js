4251f37a4d215c1c981d484943163a0840413be8
Lemaon972 <101508613+Lemaon972@users.noreply.github.com>
2022-07-15 21:16:08 +0200

    up middleware auth

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const auth = require("../middleware/auth");
+
 const sauceCtrl = require("../controllers/sauce");
 
-router.post("/", sauceCtrl.creatThing);
+router.post("/", auth, sauceCtrl.creatThing);
+router.get("/", auth, sauceCtrl.getAllThing);
+router.put("/:id", auth, sauceCtrl.modifyThing);
+router.delete("/:id", auth, sauceCtrl.deleteThing);
+router.get("/:id", auth, sauceCtrl.getOneThing);
 
-router.get("/", sauceCtrl.getAllThing);
-router.put("/:id", sauceCtrl.modifyThing);
-router.delete("/:id", sauceCtrl.deleteThing);
-router.get("/:id", sauceCtrl.getOneThing);
 module.exports = router;
