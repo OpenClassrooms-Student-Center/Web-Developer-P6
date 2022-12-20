6d2bdd40dc3fb13280144af46551ef3dce477672
jssdestroyerman <114258377+jssdestroyerman@users.noreply.github.com>
2022-12-20 18:29:33 +0100

    étape 2 finie

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,14 +1,15 @@
-const jsonwebtoken = require("jsonwebtoken")
-
-module.exports = (req,res) => {
-    try {
-        const token = req.header.authorization.split(" ")[1]
-        const decodedToken = jsonwebtoken.verify(token, "RANDOM_TOKEN_SECRET")
-        const userId = decodedToken.userId
-        req.auth = {
-            userId: userId,
-        }
-    } catch (error) {
-        res.status(401).json({ error })
-    }
-}
+const jwt = require('jsonwebtoken');
+ 
+module.exports = (req, res, next) => {
+   try {
+       const token = req.headers.authorization.split(' ')[1];
+       const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+       const userId = decodedToken.userId;
+       req.auth = {
+           userId: userId
+       };
+	next();
+   } catch(error) {
+       res.status(401).json({ error });
+   }
+};
