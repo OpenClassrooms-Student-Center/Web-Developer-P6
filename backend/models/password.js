4a4973cda0ca12f6fe6c2337549d63163578111d
jssdestroyerman <114258377+jssdestroyerman@users.noreply.github.com>
2022-12-23 11:50:58 +0100

    Update password.js

--- backend/models/password.js
+++ backend/models/password.js
@@ -7,7 +7,7 @@ schema
 .is().max(100)                                  // Maximum length 100
 .has().uppercase()                              // Must have uppercase letters
 .has().lowercase()                              // Must have lowercase letters
-.has().digits(2)                                // Must have at least 2 digits
+.has().digits(1)                                // Must have at least 1 digits
 .has().not().spaces()                           // Should not have spaces
 
 module.exports = schema
