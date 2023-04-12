ad6d035448ff171b1e7f87a5119484892ed885aa
Morgane Fisichella <morgane.fisichella@outlook.fr>
2023-04-12 16:07:30 +0200

    add backend

---
+++ backend/middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+ 
+module.exports = (req, res, next) => {
+   try {
+       const token = req.headers.authorization.split(' ')[1];
+       const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+       const userId = decodedToken.userId;
+       req.auth = {
+           userId: userId
+       };
+	next();
+   } catch(error) {
+       res.status(401).json({ error });
+   }
+};
---
+++ backend/models/thing.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const thingSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Thing', thingSchema);
